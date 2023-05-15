52e237a10d035216c9cba14768dabbae4096440b
Nicolas Vasquez <nicolas Vasquezorrego5@gmail.com>
2023-05-15 16:32:15 -0500

    se inicio la creacion del menu en js y se finalizo el slider

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+(function(){
+    const openButton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('.nav__link');
+    const closeMenu = document.querySelector('.nav__close');
+
+    openButton.addEventListener('click',()=>{
+        alert(auch)
+    })
+
+})();
